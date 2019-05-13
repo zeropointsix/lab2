3dde24cd817315b52093d0a2d1691757e3da6950
zeropointsix <47769842+zeropointsix@users.noreply.github.com>
2019-05-13 23:56:39 +0300

    Update shownum_test.h

--- tests/shownum_test.h
+++ tests/shownum_test.h
@@ -8,8 +8,16 @@ extern "C" {
 
 
 TEST(shownum_test, emptyfile) {
-    SUCCEED();
+    FILE *outputFile;
+    TRAVIS ? outputFile = fopen("tests/output/output1.txt", "wb") : outputFile = fopen("../../testing2/tests/output/output1.txt", "wb");
+    if (outputFile == NULL) {
+        printf("Cannot open file for output");
+        FAIL();
+    }
+    else {
+        fclose(outputFile);
+        SUCCEED();
+    }
 }
-
 
 #endif // SHOWUPPER_TEST_H
