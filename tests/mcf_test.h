14cf1ce7bff8f2bcc7e97ab6cdc080581fdc7a1a
zeropointsix <47769842+zeropointsix@users.noreply.github.com>
2019-05-14 16:26:02 +0300

    Update mcf_test.h

--- tests/mcf_test.h
+++ tests/mcf_test.h
@@ -78,20 +78,20 @@ TEST(mcf_test, next_line) {
         printf("Cannot open file for output. ");
         FAIL();
     }
-    //int oldstdOut = changeStream(outputFile);
+    int oldstdOut = changeStream(outputFile);
 
     text txt = create_text();
     char inFile[MAXLINE];
     TRAVIS ? strncpy(inFile, "tests/input/input12.txt", MAXLINE) : strncpy(inFile, "../../lab2/tests/input/input12.txt", MAXLINE);
     load(txt, inFile);
 
-    mwcrsr(txt, 1, 9);
+    mwcrsr(txt, 1, 10);
     mcf(txt);
     mcf(txt);
     mcf(txt);
     show(txt);
 
-    //returnStream(outputFile, oldstdOut);
+    returnStream(outputFile, oldstdOut);
 
     FILE *expectedData;
     TRAVIS ? expectedData = fopen("tests/expected/expected12.txt", "r") : expectedData = fopen("../../lab2/tests/expected/expected12.txt", "r");
