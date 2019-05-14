34db8c29e5d21d1d728232158f23f2071ae3740a
zeropointsix <47769842+zeropointsix@users.noreply.github.com>
2019-05-14 12:21:35 +0300

    Update shared.h

--- tests/shared.h
+++ tests/shared.h
@@ -40,6 +40,8 @@ int executeTest(FILE* expectedData, FILE* outputData) {
     char outputLine[MAXLINE];
 
     while ((fgets(expectedLine, MAXLINE, expectedData) != NULL) | (fgets(outputLine, MAXLINE, outputData) != NULL)) {
+        printf("%s", expectedLine);
+        printf("%s", outputLine);
         expectedLine[strlen(expectedLine) - 1] = '\0';
         outputLine[strlen(outputLine) - 1] = '\0';
         printf("Expected line: \"%s\", output line: \"%s\" \n", expectedLine, outputLine);
